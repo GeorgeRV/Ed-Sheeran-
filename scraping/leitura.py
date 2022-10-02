b9d5ff78ab3451580a19b127ecb17d07162a4a6e
GeorgeRV <georgebada@hotmail.com>
2022-10-02 13:39:40 -0300

    Atualize leitura.py

--- scraping/leitura.py
+++ scraping/leitura.py
@@ -11,23 +11,26 @@ def scrap_albuns(url):
     tabela = pagina.find_all("table", attrs = {"class" : id_tabela})
 
     table = pd.read_html(str(tabela))
-
     allas = pd.DataFrame({"Title": [], "Length": []})
     print(len(table))
     for versoes in table: 
-        tempo = versoes[["Title", "Length"]].copy()
+        tempo = versoes[["Title", "Length"]].copy().dropna()
         tempo.set_index("Title", inplace = True)
         try:
             allas = allas.merge(tempo, how = "outer", on = ["Title", "Length"])
         except:
             print("deu ruim")
-        print("entao vamos \n\n",allas)
+        #print("entao vamos \n\n",allas)
     #allas.drop("Total length:", inplace = True)
     return allas
 
 
 url = "https://en.wikipedia.org/wiki/%2B_(album)"
-print("\n\n\nBagulho:", scrap_albuns(url))
+plus = scrap_albuns(url)
+print("\n\n\nBagulho:", plus, "\n\n")
+lis = []
+lis += plus["Title"][16]
+print(type(plus["Title"][16]), plus["Title"][16], lis)
 
 url = "https://en.wikipedia.org/wiki/X_(Ed_Sheeran_album)"
 #print("\n\n\nBagulho:", scrap_albuns(url))
