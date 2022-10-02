fc8fa3a0e403c6d8b665c2f5bec3b58f52ccacd3
GeorgeRV <georgebada@hotmail.com>
2022-10-02 12:55:07 -0300

    Atualize perguntas_2.py

--- modulos/perguntas_2.py
+++ modulos/perguntas_2.py
@@ -2,26 +2,51 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_excel("A1 LP.xlsx")
-df.drop(["Unnamed: 0", "Artista", "Lyric"], axis=1, inplace=True)
+df.drop(["Unnamed: 0", "Artista"], axis=1, inplace=True)
 df.sort_values(by = "Album", inplace = True)
-
-#df.rename(columns = {"": "Nome"}, inplace=True)
 df.set_index(["Album", "Música"], inplace = True)
-
 
 pd.set_option("display.max_rows", 500)
 pd.set_option("display.min_rows", 500)
 
-musicas = df.index.values
-lis = []
-for musica in musicas:
-    lis += musica[1].split()
-uni = {}
-for palavra in lis:
-    uni[palavra] = lis.count(palavra)
-uni = pd.Series(uni)
 
-print(uni.sort_values(ascending=False))
+def palavras_comuns_titalbum(df):
+    musicas = df.index.values
+    lis = np.array()
+    for album in musicas:
+        lis[album[0]] = ""
+        #print("\nAlbum: \n", album[0])
+        #print("\nLis: \n", lis)
+    print("\nlis\n\n", lis)
+    uni = {}
+    for palavra in lis.keys:
+        uni[palavra] = lis.count(palavra)
+    uni = pd.Series(uni)
+    uni.sort_values(ascending=False, inplace = True)
+    return uni
+
+
+def palavras_comuns_titmusicas(df):
+    musicas = df.index.values
+    lis = []
+    for musica in musicas:
+        lis += musica[1].split().lower()
+        print(type(musica[1].split()))
+    uni = {}
+    for palavra in lis:
+        uni[palavra] = lis.count(palavra)
+    uni = pd.Series(uni)
+    uni.sort_values(ascending=False, inplace = True)
+    return uni
+
+
+
+
+print("-"*60)
+print(palavras_comuns_titmusicas(df))
+
+print("-"*60)
+#print(palavras_comuns_titalbum(df))
 
 
 # indices = ["Shape of You", "Perfect", "Castle on the Hill", "Thinking Out Loud"]
